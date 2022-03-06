252fac9eb9f75532065e7c3282339df8867d020f
NgLinhhh <80861130+NgLinhXko@users.noreply.github.com>
2022-03-06 22:42:15 +0700

    SQL and backend

---
+++ backend/index.js
@@ -0,0 +1,129 @@
+// import all package
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+
+//connect mysql database
+const db = mysql.createConnection({
+        host: 'localhost',
+        user:'root',
+        password:'ngoclinh741',
+        database:'e4k',
+        port:3306
+});
+//check database Connection
+db.connect(err=>{
+    if(err) {console.log(err,'dberr');}
+    console.log('database connected ...');
+})
+
+//get all data
+app.get('/user',(req,res)=>{
+    let qr = 'select * from user';
+    db.query(qr,(err,result)=>{
+        if(err)
+        {
+            console.log(err,'errs');
+        }
+        if(result.length>0){
+            res.send({
+                    message: 'all user data',
+                    data: result
+            });
+        }
+    });
+
+});
+//get single data
+app.get('/user/:id',(req,res)=>{
+       //console.log(req.params.id,'getid==>');
+       let gID = req.params.id;
+       let qr = `select * from user where id = ${gID}`;
+       db.query(qr,(err,result)=>{
+           if(err){
+               console.log(err);
+           }
+           if(result.length>0){
+            res.send({
+                    message: 'get single data',
+                    data: result
+            });
+        }
+           else{
+               res.send({
+                   message:'data not found'
+               });
+           }
+       });
+
+});
+//create data
+app.post('/user',(req,res)=>{
+    //console.log('postdata');
+    console.log(req.body,'createdata');
+
+    let name = req.body.fullname;
+    let email = req.body.email;
+    let mobile = req.body.mobile;
+
+    let qr = `insert into user(fullname,email,mobile)
+                values('${name}','${email}','${mobile}')`;
+    
+    db.query(qr,(err,result)=>{
+
+        if(err){console.log(err);}
+        console.log(result,'result')
+
+        res.send({
+            message:'data inserted'
+        });
+    });
+});
+
+// update single data
+
+app.put('/user/:id',(req,res)=>{
+    console.log(req.body,'updatedata');
+
+    let gID = req.params.id;
+    let name = req.body.fullname;
+    let email = req.body.email;
+    let mobile = req.body.mobile;
+
+    let qr =  `update user set fullname = '${name}', email = '${email}', mobile='${mobile}'
+                where id = ${gID}`;
+    db.query(qr,(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send({
+            message:'data updated'
+        })
+    })
+
+})
+
+//delete single data
+
+app.delete('/user/:id',(req,res)=>{
+    let qID = req.params.id;
+    let qr = `delete from user where id = '${qID}'`;
+    db.query(qr,(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        res.send({
+            message: 'data deleted'
+        })
+    })
+})
+
+//running server
+app.listen(3000,()=>{
+    console.log('server running..');
+})
---
+++ e4k.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 05, 2022 lúc 07:14 AM
+-- Phiên bản máy phục vụ: 10.4.20-MariaDB
+-- Phiên bản PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `e4k`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `fullname`, `email`, `mobile`) VALUES
+(1, 'linhne', 'linh@gmail.com', '090909'),
+(2, 'pho', 'pho@gmail.com', '099990'),
+(3, 'chos', 'chos@gmail.com', '099997');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
